893ebf01feb6532d1f34d1a3fcebd77df42bcf2e
JIA <jiafa@hello-happy.world>
2026-09-21 23:59:11 +0800

    feat: Display Delight 餐盘与 Functional Storage 压缩的品质传播

--- src/main/java/de/cadentem/quality_food/mixin/displaydelight/InterationManagerMixin.java
+++ src/main/java/de/cadentem/quality_food/mixin/displaydelight/InterationManagerMixin.java
@@ -1,19 +1,60 @@
 package de.cadentem.quality_food.mixin.displaydelight;
 
 import com.jkvin114.displaydelight.events.InterationManager;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import de.cadentem.quality_food.core.attachments.LevelData;
+import de.cadentem.quality_food.core.codecs.Quality;
+import de.cadentem.quality_food.util.QualityUtils;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(InterationManager.class)
 public abstract class InterationManagerMixin {
-//    @Inject(method = "tryPlaceItem")
+    @Inject(method = "tryTakeItemWithBareHand", at = @At("HEAD"), remap = false)
+    private static void quality_food$storeQuality(Player player, ServerLevel world, BlockHitResult rez, CallbackInfoReturnable<Boolean> callback, @Share("bareHandQuality") LocalRef<Quality> quality) {
+        quality.set(LevelData.get(world, rez.getBlockPos()));
+    }
 
-    /** TODO
-     * - level data for itemstack list if quality > 0
-     *     - need to display info in jade?
-     * - InterationManager#tryPlaceItem... -> store itemstack in level data
-     * - InterationManager#tryTakeItemWithBareHand -> retrieve stored itemstack
-     * - technically need to store it in item data as well when picked up in creative etc. and mixed exist?
-     *
-     * */
+    @ModifyArg(method = "tryTakeItemWithBareHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;add(Lnet/minecraft/world/item/ItemStack;)Z"))
+    private static ItemStack quality_food$applyQualityToInventoryItem(ItemStack stack, @Local(argsOnly = true) ServerLevel world, @Local(argsOnly = true) BlockHitResult rez, @Share("bareHandQuality") LocalRef<Quality> quality) {
+        QualityUtils.applyQuality(stack, quality.get());
+        return stack;
+    }
+
+    @ModifyArg(method = "tryTakeItemWithBareHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setItemInHand(Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;)V"), index = 1)
+    private static ItemStack quality_food$applyQualityToHandItem(ItemStack stack, @Local(argsOnly = true) ServerLevel world, @Local(argsOnly = true) BlockHitResult rez, @Share("bareHandQuality") LocalRef<Quality> quality) {
+        QualityUtils.applyQuality(stack, quality.get());
+        return stack;
+    }
+
+    /** Reject stacking items whose quality does not match the plated food */
+    @Inject(method = "tryPlaceItemOnPlate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z", shift = At.Shift.AFTER), cancellable = true)
+    private static void quality_food$checkQuality(Player player, ServerLevel world, BlockHitResult rez, boolean isMainHand, CallbackInfoReturnable<Boolean> callback, @Local ItemStack handStack) {
+        Quality blockQuality = LevelData.get(world, rez.getBlockPos());
+        Quality itemQuality = QualityUtils.getQuality(handStack);
+        if (!(blockQuality == itemQuality || (blockQuality == Quality.PLAYER_PLACED && itemQuality == Quality.NONE))) {
+            callback.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = {"tryPlaceItemOnPlate", "tryPlaceItemOnSmallPlate"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", shift = At.Shift.AFTER))
+    private static void quality_food$applyQuality(Player player, ServerLevel world, BlockHitResult rez, boolean isMainHand, CallbackInfoReturnable<Boolean> callback, @Local ItemStack handStack) {
+        Quality quality = QualityUtils.getQuality(handStack);
+        LevelData.set(world, rez.getBlockPos(), quality != Quality.NONE ? quality : Quality.PLAYER_PLACED);
+    }
+
+    @Inject(method = "tryPlaceItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;swing(Lnet/minecraft/world/InteractionHand;Z)V"))
+    private static void quality_food$applyQualityOnDisplay(Player player, ServerLevel world, BlockHitResult rez, boolean isMainHand, CallbackInfoReturnable<Boolean> callback, @Local ItemStack stack) {
+        Quality quality = QualityUtils.getQuality(stack);
+        LevelData.set(world, rez.getBlockPos(), quality != Quality.NONE ? quality : Quality.PLAYER_PLACED);
+    }
 }
